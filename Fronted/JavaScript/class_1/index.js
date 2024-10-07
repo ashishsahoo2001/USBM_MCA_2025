c00e9ee6e1e8b2e0c686c460e5cde585ce8ff5b7
ashishsahoo2001 <160223885+ashishsahoo2001@users.noreply.github.com>
2024-10-07 11:24:22 +0530

    Ashish

--- Fronted/JavaScript/class_1/index.js
+++ Fronted/JavaScript/class_1/index.js
@@ -1,7 +1,30 @@
-console.log("jay jagannath ")
-let a=99078;
-console.log(a);
-var b=44289;
-console.log(b);
-const c=1700
-console.log(c)
+console.log("jay jagannath ");
+//let a=99078;
+// console.log(a);
+// var b=44289;
+// console.log(b);
+// const c=1700
+// console.log(c)
+// console.log(Name);
+// var Name="Ashish Sahoo";
+// console.log(Name);
+// console.log(typeof Name);
+// let data;
+// console.log(data);
+// let num1=50;
+// let decimal_no = 1.0001;
+// let pi =3.14;
+// let age =25;
+// let roll_no=21;
+// console.log(typeof num1);//Number
+//Addition
+let num2=100;
+let num3=125;
+let addition = num2 +num3;
+console.log(addition);
+let sub = num2 - num3;
+console.log(sub);
+let mul = num2*num3;
+console.log(mul);
+let div=num2/num3;
+console.log(div);
