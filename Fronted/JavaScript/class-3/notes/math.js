a70370efbcfff179eb40452d2ae7bd84c73988e4
ashishsahoo2001 <160223885+ashishsahoo2001@users.noreply.github.com>
2024-10-08 12:35:20 +0530

    Class-3 Work

--- Fronted/JavaScript/class-3/notes/math.js
+++ Fronted/JavaScript/class-3/notes/math.js
@@ -23,15 +23,128 @@
  
 //Falsy  value 
 // false,0,-0,"",``,'',null,undefined,NaN
-let total_bill = 1000;
-let discount = 200;
-if(total_bill>500)
-{
-  console.log("You will get a Discount.");
-  console.log(`Discount is ${discount}`);
-  total_bill=total_bill-discount;
-  console.log(`Your total bill is ${total_bill}`);
-}
-else {
-  console.log("Sorry");
+// let total_bill = 1000;
+// let discount = 200;
+// if(total_bill>500)
+// {
+//   console.log("You will get a Discount.");
+//   console.log(`Discount is ${discount}`);
+//   total_bill=total_bill-discount;
+//   console.log(`Your total bill is ${total_bill}`);
+// }
+// else {
+//   console.log("Sorry You have no Discount");
+// }
+//Question-2
+// let age = 18;
+// if(age >= 60)
+// {
+// console.log("Senior and adult");
+// }
+// else if (age >= 18)
+// {
+//   console.log("Adult");
+// }
+// else {
+//   console.log("Minor");
+// }
+//Question-3
+// let Gender = "Male";
+// let age = 22;
+// if (Gender==="Male" )
+// {
+//   if(age>=22){
+//   console.log("He can able to marry");}
+// }
+// else{
+//   console.log("Not marry");
+// }
+
+//Question-4
+// let char ="a";
+// switch (char){
+//   case "a":
+//     console.log("It's a vowel");
+//     break;
+//     case "e":
+//     console.log("It's a vowel");
+//     break;
+//     case "i":
+//     console.log("It's a vowel");
+//     break;
+//     case "o":
+//     console.log("It's a vowel");
+//     break;
+//     case "u":
+//     console.log("It's a vowel");
+//     break;
+//     default:
+//     console.log("It's not a vowel");
+// }
+
+//Question-4 to check a month
+// let month = 11;
+// switch(month){
+// case 0:
+//   console.log("January");
+//   break;
+//   case 1:
+//   console.log("February");
+//   break;
+//   case 2:
+//   console.log("March");
+//   break;
+//   case 3:
+//   console.log("April");
+//   break;
+//   case 4:
+//   console.log("May");
+//   break;
+//   case 5:
+//   console.log("June");
+//   break;
+//   case 6:
+//   console.log("July");
+//   break;
+//   case 7:
+//   console.log("August");
+//   break;
+//   case 8:
+//   console.log("September");
+//   break;
+//   case 9:
+//   console.log("October");
+//   break;
+//   case 10:
+//   console.log("November");
+//   break;
+//   case 11:
+//   console.log("December");
+//   break;
+//   default:
+//     console.log("Not a Month");
+
+// }
+// Question-5 to check a day
+let week_no=1;
+switch(week_no){
+  case 0:
+    console.log("Sunday");
+    break;
+    case 1:
+    console.log("Monday");
+    break;case 2:
+    console.log("Tuesday");
+    break;case 3:
+    console.log("Wednesday");
+    break;case 4:
+    console.log("Thrusday");
+    break;case 5:
+    console.log("Friday");
+    break;
+    case 6:
+    console.log("Saturday");
+    break;
+    default:
+      console.log("enter valid number");
 }
