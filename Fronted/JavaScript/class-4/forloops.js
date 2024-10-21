448de9738a4fc61216180833f4d80d502870814a
ashishsahoo2001 <160223885+ashishsahoo2001@users.noreply.github.com>
2024-10-21 11:52:25 +0530

    forloops

---
+++ Fronted/JavaScript/class-4/forloops.js
@@ -0,0 +1,23 @@
+// let sum =0;
+// for (let i=5;i>=1;i--){
+//   sum+=i;
+  
+// }
+// console.log(sum);
+
+//   sum=0;
+//   let result;
+//   for(let j=0;j<5;j++){
+// let random_number = Math.floor(Math.random()*10);
+// console.log("Random number",random_number);
+//  sum+=random_number;
+
+// }
+// console.log(`The sum of the numbers is `,sum);
+
+for(let i=0;i<=3;i++){
+  for(let j=1;j<=2;j++){
+    console.log(j)
+  }
+  console.log(i);
+}
