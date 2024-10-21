415d376f888419303603a20e294b259e1cb489ab
ashishsahoo2001 <160223885+ashishsahoo2001@users.noreply.github.com>
2024-10-21 22:39:00 +0530

    Change

--- Fronted/JavaScript/class-4/forloops.js
+++ Fronted/JavaScript/class-4/forloops.js
@@ -5,15 +5,16 @@
 // }
 // console.log(sum);
 
-//   sum=0;
-//   let result;
-//   for(let j=0;j<5;j++){
-// let random_number = Math.floor(Math.random()*10);
-// console.log("Random number",random_number);
-//  sum+=random_number;
+  sum=0;
+  //let result;
+  for(let j=0;j<5;j++){
+let random_number = Math.floor(Math.random()*5);
+console.log("Random number is",random_number);
+ let result =sum+random_number;
+ sum+=result;
+ console.log(`The sum of the numbers is ${result}`);
 
-// }
-// console.log(`The sum of the numbers is `,sum);
+}
 
 // for(let i=0;i<=3;i++){
 //   for(let j=1;j<=2;j++){
@@ -21,10 +22,10 @@
 //   }
 //   console.log(i);
 // }
-for(let i=1;i<=5;i++){
-  let pattern ="";
-  for(let j=1;j<=i;j++){
-   pattern += "*";
-  }
-  console.log(pattern);
-}
+// for(let i=1;i<=5;i++){
+//   let pattern ="";
+//   for(let j=1;j<=i;j++){
+//    pattern += "*";
+//   }
+//   console.log(pattern);
+// }
