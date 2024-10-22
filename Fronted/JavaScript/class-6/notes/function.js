33cd1405644236c65ce85a0f013d6d95f2777fa1
ashishsahoo2001 <160223885+ashishsahoo2001@users.noreply.github.com>
2024-10-22 14:05:55 +0530

    class-6

---
+++ Fronted/JavaScript/class-6/notes/function.js
@@ -0,0 +1,40 @@
+// //Function
+
+// // write a function to print your name.
+// function printName(){
+//   console.log(`My name is Ashish`);
+// }
+// printName(); //function call or invoke the function
+
+// function printName(name/*parameter*/){
+//   console.log(`My name is ${name}`);
+// }
+
+// printName("Ashish"/*arguments*/); 
+// // function call or invoke the function with argument
+
+// function printName(name,age,city){
+// console.log(`My name is ${name}, I am ${age} years old, and I live in ${city}`);
+// }
+// printName("Ashish"); // function call or invoke the function with arguments
+// function printName(name,age,city){
+// return `My name is ${name}, I am ${age} years old, and I live in ${city}`;
+// console.log("I am stop here");
+
+// }
+// let output = printName("Ashish",23,"bbsr");
+// console.log(output); 
+
+
+//what is arrow function?
+//Syntax:
+//console.log(printName("Ashish")); 
+//=>fat arrow function
+ const printName = (name) => {
+return `My name is ${name}`;
+}
+console.log(printName("Ashis"));
+
+//Done with the basic function 
+//callback
+//Hof,promise,
