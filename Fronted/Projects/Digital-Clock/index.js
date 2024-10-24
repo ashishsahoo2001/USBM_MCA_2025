46fea5ee2b85bd31a856f58e2e02116d022c3262
ashishsahoo2001 <160223885+ashishsahoo2001@users.noreply.github.com>
2024-10-24 08:06:11 +0530

    Digital clock

--- Fronted/Projects/Digital-Clock/index.js
+++ Fronted/Projects/Digital-Clock/index.js
@@ -1,15 +1,20 @@
-function updateClock(){
-  const clock = document.getElementById("clock");
+function updateClock() {
+ 
 
-  const now = new Date();
-  console.log(clock);
+  let now = new Date();
   
   let hours = now.getHours();
   let minutes = now.getMinutes();
   let seconds = now.getSeconds();
-  clock.textContent = `${hours}:${minutes}:${seconds}`
-  
+
+  // Add leading zeros if necessary
+  hours = hours < 10 ? '0' + hours : hours;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+
+ clock.textContent = `${hours}:${minutes}:${seconds}`;
 }
+
 updateClock();
-setInterval(updateClock,1000)
+setInterval(updateClock, 1000);
  
